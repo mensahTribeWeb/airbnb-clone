b923bb41a4436e8ffe46bf5b033cef7aa2626005
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-02 02:00:34 -0500

    hero component with CSS complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './components/NavBar';
 import './App.css';
+import Hero from './components/Hero';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import heroImg from "../img/airbnb-hero.png"
+
+
+export default function Hero () {
+  return (
+    <div className="hero">
+      <img className="hero--img" src={heroImg}alt="" />
+      <h1 className="hero--title">Online Experiences</h1>
+      <p className="hero--p">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home </p>
+    </div>
+  );
+}
