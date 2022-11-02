0202a15a2f687e4c9d377431c77ec31a8e80ec8d
mensahTribeWeb <ndm.acct85@gmail.com>
2022-11-02 01:05:35 -0500

    nav component with CSS complete

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../img/airbnb.png";
+
+export default function navBar() {
+  return(
+    <nav className="nav">
+      <img className="nav--logo" src={logo} alt="airBnb logo" />
+    </nav>
+  )
+}
